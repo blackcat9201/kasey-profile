9538130cddb0ad325ab896e9b1743b1e605e463c
Kim Sangwha <kasey@snuev.com>
2013-04-17 19:55:38 +0900

    first command

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kaseyprofile::Application.config.secret_token = '36a1b712960ad04970d34f1fcafee8f11af28ce48ee9cd43298cf48e0d9ff6f3965ae2f95406f66d2d0c9d7720003d00481a89e52f44c73fe3d5b82cc9469f76'
